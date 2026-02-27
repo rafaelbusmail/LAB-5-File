e3b07be5850dc6101fe4f4272a3a540083a90067
Rafael Busmail <rafaelbusmail12@gmail.com>
2026-02-27 09:20:17 -0600

    Esqueleto de funciones

--- LAB #5 File/src/CMD/Comandos1.java
+++ LAB #5 File/src/CMD/Comandos1.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package CMD;
+import java.io.IOException;
+import java.io.File;
 
-/**
- *
- * @author jerem
- */
 public class Comandos1 {
     
+    private File carpetaActual;
+    public Comandos1(String rutaInicial) {
+        this.carpetaActual = new File(rutaInicial);
+        if (!carpetaActual.exists()) {
+            carpetaActual.mkdirs();
+        }
+        
+    }
+    
+    public String getCarpetaActual() {
+        return carpetaActual.getAbsolutePath();
+    }
+    
+    public String mkdir(String nombreCarpeta) {
+        return;
+    }
+    
+    public String mfile (String nombreArchivo) {
+        
+    }
+    
+    public String rm(String nombre) {
+        
+    }
+    
+    public String cd (String nombreCarpeta) {
+        
+    }
 }
